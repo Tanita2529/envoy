3b30ea64f022f12650058290f87155f1bbb9c7fa
Lizan Zhou <lizan@tetrate.io>
2019-11-01 21:43:44 +0000

    build nghttp2
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/windows_ci_steps.ps1
+++ ci/windows_ci_steps.ps1
@@ -1,1 +1,5 @@
+$ErrorActionPreference = "Stop";
+
+bazel --bazelrc=windows\.bazelrc build //bazel/foreign_cc:nghttp2 //bazel/foreign_cc:event;
+
 bazel --bazelrc=windows\.bazelrc test @envoy_api//test/build/...
